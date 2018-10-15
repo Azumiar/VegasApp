38c7b8e17b269cf120adcd5ffb27215c3b070e0b
Azumiar <azumiar@yandex-team.ru>
2018-10-15 21:38:46 +0300

    Пофиксил табы

--- components/AsyncImage.js
+++ components/AsyncImage.js
@@ -1,52 +1,46 @@
-
 import React, {
-    Component,
+  Component,
 } from 'react'
-  
+
 import {
-    Image,
-    View,
-    ActivityIndicator,
-    StyleSheet,
+  Image,
+  View,
+  ActivityIndicator,
+  StyleSheet,
 } from 'react-native'
-  
-  
-  export default class AsyncImage extends Component {
-  
-    constructor(props) {
-      super(props)
-      this.state = { loaded: false }
-    }
-  
-    render() {  
-      return (
-        <View>
-          <Image
-            source={this.props.source}
-            style={[this.props.style,{display: this.state.loaded ? "flex" : "none",}]}
-            onLoad={this._onLoad} />
 
-          {!this.state.loaded &&
-            <View style={this.styles.view}>
-              <ActivityIndicator size={75} color="#C3073F" />
-            </View>
-          }          
-        </View>
-      )
-    }
-  
-    _onLoad = () => {
-      this.setState(() => ({ loaded: true }))
-    } 
-
-    styles = StyleSheet.create({
-      view:{
-        justifyContent: "center",
-        height: this.props.style.height,
-        width: this.props.style.width,
-      },
-    })
+export default class AsyncImage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loaded: false }
   }
 
+  render() {  
+    return (
+      <View>
+        <Image
+          source={this.props.source}
+          style={[this.props.style,{display: this.state.loaded ? "flex" : "none",}]}
+          onLoad={this._onLoad} />
 
-  
+        {!this.state.loaded &&
+          <View style={this.styles.view}>
+            <ActivityIndicator size={75} color="#C3073F" />
+          </View>
+        }          
+      </View>
+    )
+  }
+
+  _onLoad = () => {
+    this.setState(() => ({ loaded: true }))
+  } 
+
+  styles = StyleSheet.create({
+    view:{
+      justifyContent: "center",
+      height: this.props.style.height,
+      width: this.props.style.width,
+    },
+  })
+}
