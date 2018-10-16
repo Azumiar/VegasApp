74213955031ef70cef9d7f45e20e350130cab7d0
Azumiar <azumiar@yandex-team.ru>
2018-10-16 22:38:05 +0300

    Updated MenuScreen

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {
-  Image,
   ScrollView,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 
 import {
   ButtonGroup,
-  Button,
 } from 'react-native-elements'
 
 import AsyncImage from '../components/AsyncImage'
@@ -39,111 +36,50 @@ export default class MenuScreen extends React.Component {
     }
   };
 
-
-
   render() {
-    const buttons = ['Напитки', 'Десерты', 'Еда']
-    const { selectedIndex } = this.state
+    const buttons = ['Напитки', 'Десерты', 'Еда'];
+    const { selectedIndex } = this.state;
 
-    if(this.state.selectedIndex == 0){
-      return (
-            <View style={styles.container}>
-              <View style={styles.menu}>
-              <ButtonGroup
-                onPress={this.updateIndex}
-                selectedIndex={selectedIndex}
-                buttons={buttons} 
-                containerStyle={{
-                 height: 50, width: "100%", borderRadius: 0, borderWidth: 0,
-                 marginLeft : 0, marginTop: 0, borderColor: '#C3073F', borderWidth: 2,
-                }}
-                innerBorderStyle={{width: 2, color: '#C3073F' }}
-                buttonStyle={{ backgroundColor: '#29292e',  }}
-                selectedTextStyle={{color: '#C3073F', }}
-                selectedButtonStyle={{ backgroundColor: '#1A1A1D',  }}
-              />
-              </View>  
-              <ScrollView style={styles.scroll}>
-                {/*<Image style={styles.cardImage} source={{ uri: 'https://pp.userapi.com/c830609/v830609322/603c7/JayPNsKGe5M.jpg'}}/>*/}
-                <AsyncImage
-                  style={{
-                    width: '100%',
-                    height: 300,
-                    borderRadius: 15,
-                    marginTop: 30,
-                    marginBottom: 15,
-                  }}
-                  source={{
-                    uri: 'https://pp.userapi.com/c830609/v830609322/603c7/JayPNsKGe5M.jpg'
-                  }}
-                  />
-              </ScrollView>
-            </View>
-          );
-    }
-
-    if(this.state.selectedIndex == 1){
-      return (
-        <View style={styles.container}>
-          <View style={styles.menu}>
-          <ButtonGroup
-                onPress={this.updateIndex}
-                selectedIndex={selectedIndex}
-                buttons={buttons} 
-                containerStyle={{
-                 height: 50, width: "100%", borderRadius: 0, borderWidth: 0,
-                 marginLeft : 0, marginTop: 0, borderColor: '#C3073F', borderWidth: 2,
-                }}
-                innerBorderStyle={{width: 2, color: '#C3073F' }}
-               
-                buttonStyle={{ backgroundColor: '#29292e',  }}
-                selectedTextStyle={{color: '#C3073F', }}
-                selectedButtonStyle={{ backgroundColor: '#1A1A1D',  }}
-              />
-          </View>  
-          <ScrollView style={styles.scroll}>
-            <Image style={styles.cardImage} source={{ uri: 'https://pp.userapi.com/c830609/v830609322/603be/QhQhNzN_YgE.jpg'}}/>
-          </ScrollView>
-        </View>
-      );
-    }
-
-    if(this.state.selectedIndex == 2){
-      return (
-        <View style={styles.container}>
-          <View style={styles.menu}>
-          <ButtonGroup
-                onPress={this.updateIndex}
-                selectedIndex={selectedIndex}
-                buttons={buttons} 
-                containerStyle={{
-                 height: 50, width: "100%", borderRadius: 0, borderWidth: 0,
-                 marginLeft : 0, marginTop: 0, borderColor: '#C3073F', borderWidth: 2,
-                }}
-                innerBorderStyle={{width: 2, color: '#C3073F' }}
-               
-                buttonStyle={{ backgroundColor: '#29292e',  }}
-                selectedTextStyle={{color: '#C3073F', }}
-                selectedButtonStyle={{ backgroundColor: '#1A1A1D',  }}
-              />
-          </View>  
-          <ScrollView style={styles.scroll}>
-            <Image style={styles.cardImage} source={{ uri: 'https://pp.userapi.com/c841532/v841532322/6139f/g1xlVELUyRA.jpg'}}/>
-            <Image style={styles.cardImage} source={{ uri: 'https://pp.userapi.com/c841532/v841532322/613a8/LDppinAVKiM.jpg'}}/>
-          </ScrollView>
-        </View>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.menu}>
+        <ButtonGroup
+          onPress={this.updateIndex}
+          selectedIndex={selectedIndex}
+          buttons={buttons} 
+          containerStyle={{
+            height: 50, width: "100%", borderRadius: 0, borderWidth: 0,
+            marginLeft : 0, marginTop: 0, borderColor: '#C3073F', borderWidth: 2,
+          }}
+          innerBorderStyle={{width: 2, color: '#C3073F' }}
+          buttonStyle={{ backgroundColor: '#29292e',  }}
+          selectedTextStyle={{color: '#C3073F', }}
+          selectedButtonStyle={{ backgroundColor: '#1A1A1D',  }}
+        />
+        </View>  
+        <ScrollView style={styles.scroll}>
+          { this.state.selectedIndex == 0 && (
+              <AsyncImage style={styles.cardImage} source={{uri: 'https://pp.userapi.com/c830609/v830609322/603c7/JayPNsKGe5M.jpg'}}/>
+            ) || this.state.selectedIndex == 1 && (
+              <AsyncImage style={styles.cardImage} source={{uri: 'https://pp.userapi.com/c830609/v830609322/603be/QhQhNzN_YgE.jpg'}}/>
+            ) || this.state.selectedIndex == 2 && (
+              <View>
+                <AsyncImage style={styles.cardImage} source={{uri: 'https://pp.userapi.com/c841532/v841532322/6139f/g1xlVELUyRA.jpg'}}/>
+                <AsyncImage style={styles.cardImage} source={{uri: 'https://pp.userapi.com/c841532/v841532322/613a8/LDppinAVKiM.jpg'}}/>
+              </View>
+            )
+          }
+        </ScrollView>
+      </View>
+    );
   }
 
-onPressHandler = (type) => {
+  onPressHandler = (type) => {
     return () => {
       this.setState({ currentScreen: type });
     }
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
