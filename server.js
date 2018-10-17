ded461612fc03ac884a150cd2bde14673089056f
Azumiar <azumiar@yandex-team.ru>
2018-10-17 10:13:01 +0300

    Added admin panel

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const router = require('./server/router')
 
 app.use('/', router);
 
+app.use(express.static('server/admin'));
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 }); 
--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ const Users = require('./controllers/users');
 const News = require('./controllers/news');
 
 router.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.sendFile(__dirname +  '/admin/index.html');
 });
 
 router.get('/user', Users.getUser);
